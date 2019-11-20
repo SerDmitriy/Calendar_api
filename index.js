b826759389a28456c5127395360f58484266c5b9
Artem <user@Artem-MacBook-Pro.local>
2019-11-20 18:29:57 +0200

    Koa with commented express

--- index.js
+++ index.js
@@ -1,3 +1,61 @@
+const Koa = require('koa')
+const KoaRouter = require('koa-router')
+const json = require('koa-json')
+const path = require('path')
+const render = require ('koa-ejs')
+
+const app = new Koa()
+const router = new KoaRouter()
+const bodyParser = require('koa-bodyparser')
+
+const calendars = ['Birth days', 'Partys', 'Religion']
+
+app.use(json())
+app.use(bodyParser())
+// app.use(async ctx => (ctx.body = { msg: 'Hello World'}))
+
+app.context.name = "Bob"
+
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: 'layout',
+  viewExt: 'html',
+  cache: false,
+  debug: false,
+})
+
+router.get('/', index)
+router.get('/add', showAdd)
+router.post('/add', add)
+
+
+
+async function index(ctx) {
+  await ctx.render('index', {
+    title: 'Calendar',
+    calendars: calendars,
+  })
+}
+
+async function showAdd(ctx) {
+  await ctx.render('add')
+}
+
+async function add(ctx) {
+  const body = ctx.request.body
+  calendars.push(body.calendar)
+  ctx.redirect('/')
+}
+
+router.get('/test', ctx => (ctx.body = `Hello ${ctx.name}`))
+router.get('/test2/:name', ctx => (ctx.body = `Hello ${ctx.params.name}`))
+
+
+app.use(router.routes()).use(router.allowedMethods())
+app.listen(3000, () => console.log('Server started...'))
+
+
+
 // const express = require('express');
 // const bodyParser = require('body-parser');
 // const cors = require('cors');
